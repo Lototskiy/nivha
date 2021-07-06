a508e8259e496252632c24138437968ea46f7611
Dima <timoha.lisyev2014@yandex.ru>
2021-07-06 10:56:24 +0300

    new project

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import './assets/css/normalize.scss';
+import './assets/css/main.scss';
+
+
+//BLOCKS 
+
+import './assets/css/blocks/btn.scss';
+import './assets/css/blocks/mobile-menu.scss';
+import './assets/css/blocks/popup.scss';
+import './assets/css/blocks/header.scss';
+import './assets/css/blocks/inputs.scss';
+import './assets/css/blocks/aside.scss';
+import './assets/css/blocks/footer.scss';
+import './assets/css/blocks/main-screen.scss';
+
+
+
+// JS
+
+import './assets/js/popup';
+import './assets/js/inputs';
+import './assets/js/inputs-mask';
+import './assets/js/header';
+import './assets/js/mobile-menu';
+import './assets/js/anchors';
