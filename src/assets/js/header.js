7cfd00cce174db4e6680c400ad5117ffb20ead27
Vls <bestblodd@gmail.com>
2021-08-02 13:03:52 +0300

    end fixes

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -1,27 +1,27 @@
-let header = document.querySelector('.header');
+// let header = document.querySelector('.header');
 
-document.addEventListener('scroll', function () {
-	let scroll = document.querySelector('html').scrollTop;
-	if (scroll > 200 & !header.classList.contains('header--fixed')) {
-		headerFix();
-	} else if (scroll < 200 & header.classList.contains('header--fixed')) {
-		headerStatic();
-	}
+// document.addEventListener('scroll', function () {
+// 	let scroll = document.querySelector('html').scrollTop;
+// 	if (scroll > 200 & !header.classList.contains('header--fixed')) {
+// 		headerFix();
+// 	} else if (scroll < 200 & header.classList.contains('header--fixed')) {
+// 		headerStatic();
+// 	}
 
-});
+// });
 
-function headerFix() {
-	header.classList.add('header--fixed');
-}
+// function headerFix() {
+// 	header.classList.add('header--fixed');
+// }
 
-function headerStatic() {
-	header.classList.add('header--height0');
+// function headerStatic() {
+// 	header.classList.add('header--height0');
 
-	function headerRemoveFix() {
+// 	function headerRemoveFix() {
 
-		header.classList.remove('header--fixed');
-		header.classList.remove('header--height0');
-		header.removeEventListener('animationend', headerRemoveFix)
-	}
-	header.addEventListener('animationend', headerRemoveFix)
-}
+// 		header.classList.remove('header--fixed');
+// 		header.classList.remove('header--height0');
+// 		header.removeEventListener('animationend', headerRemoveFix)
+// 	}
+// 	header.addEventListener('animationend', headerRemoveFix)
+// }
